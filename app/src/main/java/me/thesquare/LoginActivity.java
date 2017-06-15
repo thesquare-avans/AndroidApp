c06247dc2a8c303fb7f8bba5dc6b68e2235165d2
larsheesakkers1 <larsheesakkers@live.nl>
2017-06-15 10:24:15 +0200

    update login

--- app/src/main/java/me/thesquare/LoginActivity.java
+++ app/src/main/java/me/thesquare/LoginActivity.java
@@ -1,6 +1,7 @@
 package me.thesquare;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -39,6 +40,15 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View v) {
                 if ( checkUsername() ){
                     addUser( txtUsername.getText().toString() );
+                    Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(i);
+                }
+                else{
+                    Context context = getApplicationContext();
+                    CharSequence text = "Please fill in the login field.";
+                    int duration = Toast.LENGTH_SHORT;
+                    Toast toast =  Toast.makeText(context,text,duration);
+                    toast.show();
                 }
             }
         });
@@ -55,6 +65,7 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void addUser(String username){
+        realm = Realm.getDefaultInstance();
         RealmResults<UserModel> result = realm.where( UserModel.class ).equalTo( "username", txtUsername.getText().toString() ).findAll();
         if ( result.isEmpty() && !username.equals("") ) {
             keyManager = new KeyManager();
