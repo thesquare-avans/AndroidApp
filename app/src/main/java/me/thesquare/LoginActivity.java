c7bd8570a5faa150faf5849eae886431249ca7c3
TigerzHead <cliffsestig@gmail.com>
2017-06-20 09:56:39 +0200

    Realm fix

--- app/src/main/java/me/thesquare/LoginActivity.java
+++ app/src/main/java/me/thesquare/LoginActivity.java
@@ -96,13 +96,13 @@ public class LoginActivity extends AppCompatActivity {
                     List<UserModel> user = realm.copyFromRealm(result);
                     UserModel userModel = user.get(0);
 
-
+                    userModel.setId(id);
 
                     if(!result.isEmpty()){
                         realm.beginTransaction();
                         //Hier gaat iets mis
-                        realm.copyToRealmOrUpdate(userModel);
-                        userModel.setId(id);
+                        realm.insertOrUpdate(userModel);
+
                         realm.commitTransaction();
                         Toast.makeText(LoginActivity.this,
                                 "Gebruiker: " + userModel.getUsername() + " toegevoegd!", Toast.LENGTH_SHORT).show();
