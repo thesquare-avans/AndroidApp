71cefede565ab20108842e161efcf62a6c772924
Jens de Rond <derond@redkiwi.nl>
2017-06-21 11:43:12 +0200

    Add missing classes and remove if statement

--- app/src/main/java/me/thesquare/ApiHandler.java
+++ app/src/main/java/me/thesquare/ApiHandler.java
@@ -244,12 +244,7 @@ public class ApiHandler {
                         UserModel user = new UserModel();
                         JSONObject userData = data.getJSONObject("user");
                         user.setId(userData.getString("id"));
-                        if (user.getSatoshi() == 0) {
-                            user.setSatoshi(1);
-                        }
-                        else{
-                            user.setSatoshi(userData.getInt("satosi"));
-                        }
+                        user.setSatoshi(userData.getInt("satoshi"));
                         user.setUsername(userData.getString("name"));
                         callback.on(user);
                     } catch (JSONException e) {
---
+++ app/src/main/java/me/thesquare/apiresponses/UserResponse.java
@@ -0,0 +1,11 @@
+package me.thesquare.apiresponses;
+
+import me.thesquare.models.UserModel;
+
+/**
+ * Created by larsh on 20-6-2017.
+ */
+
+public interface UserResponse {
+    void on(UserModel userModel);
+}
